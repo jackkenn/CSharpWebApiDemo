37143c45a790792c2061263eed7e60c33ea5fa36
jackkenn <55853855+jackkenn@users.noreply.github.com>
2023-10-20 21:32:09 -0500

    9 frontend sidebar (#16)
    * added very bad side bar
    
    * sidenav and dashboard are pretty enough

--- FrontEnd/Portal/src/app/admin/admin-routing.module.ts
+++ FrontEnd/Portal/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', component: SidenavComponent },
 ];
 
 @NgModule({
